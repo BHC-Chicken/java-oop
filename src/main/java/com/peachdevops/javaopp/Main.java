fc883f40623f73aa781b9c325f992960416ae351
Park <tij05078@gmail.com>
2021-12-23 18:41:30 +0900

    Update #3 - Main
    번수 클래스를 인터페이스로 변경
    #3 마무리

--- src/main/java/com/peachdevops/javaopp/Main.java
+++ src/main/java/com/peachdevops/javaopp/Main.java
@@ -1,12 +1,13 @@
 package com.peachdevops.javaopp;
 
-import com.peachdevops.javaopp.logic.BubbleSort;
+import com.peachdevops.javaopp.logic.JavaSort;
+import com.peachdevops.javaopp.logic.Sort;
 
 import java.util.Arrays;
 
 public class Main {
     public static void main(String[] args) {
-        BubbleSort<String> bubbleSort = new BubbleSort<>();
+        Sort<String> bubbleSort = new JavaSort<>();
 
         System.out.println(bubbleSort.sort(Arrays.asList(args)));
     }
